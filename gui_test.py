0960862b6baa37d5d7c79362a179bf496bc984cd
Joe Gao <joegocod3@gmail.com>
2022-02-12 20:27:04 -0500

    added jasons library to simple layout

--- gui_test.py
+++ gui_test.py
@@ -1,9 +1,11 @@
+import sys
+from time import sleep
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-connected = False
+from python_socket_test.DroneClients import DroneClients
 
 Form, Window = uic.loadUiType("main.ui")
 
@@ -13,10 +15,22 @@ window = Window()
 form = Form()
 form.setupUi(window)
 
-def connectClicked():
-  print(connected)
 
-form.connectButton.clicked.connect(connectClicked)
+def startDroneClient():
+	map = 0  # get_map
+	telemetry = 0 #get_telemetry
+	stop_threads = False
+
+	# map object, telemetry object, SERVER, PORT
+	drone_clients = DroneClients(map, telemetry,"192.168.1.108", 5050 )
+	drone_clients.run()
+	drone_clients.send_command("START")
+	drone_clients.send_command("STOP")
+	drone_clients.send_command("ARM")
+	drone_clients.send_command("DISARM")
+	print('done')
+
+form.connectButton.clicked.connect(startDroneClient)
 
 window.show()
 app.exec_()
